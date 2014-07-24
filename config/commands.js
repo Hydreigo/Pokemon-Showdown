1e7b6dd945dfd9de69402fbcff586c24d37c25ff
Blizzard <bronyanthony@gmail.com>
2014-07-24 06:59:50 -0400

    there are two /pickrandom, so let's use Zarel's.

--- config/commands.js
+++ config/commands.js
@@ -1552,12 +1552,6 @@ var commands = exports.commands = {
 		return this.sendReply("You are now showing your authority!");
 	},
 
-	pr: 'pickrandom',
-	pickrandom: function (target, room, user) {
-		if (!this.canBroadcast()) return false;
-		return this.sendReply(target.split(',').map(function (s) { return s.trim(); }).randomize()[0]);
-	},
-
 	spam: 'spamroom',
 	spamroom: function (target, room, user) {
 		if (!target) return this.sendReply("Please specify a user.");
